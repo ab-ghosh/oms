80f9268c2ff4158e46d32fee348239bc1fbd41a4
Abhishek Ghosh <abhi.ghosh3108@gmail.com>
2025-05-24 21:26:22 +0530

    modified the code
    Signed-off-by: Abhishek Ghosh <abhi.ghosh3108@gmail.com>

--- common/go.mod
+++ common/go.mod
@@ -1,3 +1,3 @@
-module github.com/ab-ghosh/commos
+module github.com/ab-ghosh/oms/common
 
 go 1.23.6
--- gateway/main.go
+++ gateway/main.go
@@ -1,9 +1,23 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+
+	"github.com/ab-ghosh/oms/common"
+	_ "github.com/joho/godotenv/autoload"
+)
+
+var (
+	httpAddr = common.EnvString("HTTP_ADDR", ":3000")
+)
 
 func main() {
 	mux := http.NewServeMux()
 	handler := NewHandler()
 	handler.registerRoutes(mux)
+	log.Printf("Starting HTTP Server at %s", httpAddr)
+	if err := http.ListenAndServe(httpAddr, mux); err != nil {
+		log.Fatal(err)
+	}
 }
